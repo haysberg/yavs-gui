7e01cd55d0dbac9392fefd2107c53d6ab1d5e7f4
Téo Le Dozo <teo@couaque.fr>
2020-02-18 13:06:20 +0000

    Added the ping utility to the home page.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 #You'll find that this file is very small. It's only job is to initialize the Flask server,
 #and to respond to requests coming to the root URL (/)
 from flask import Flask, render_template, request
+import json, requests
 
 #We need this for the Flask server to run
 app = Flask(__name__, template_folder='templates')
@@ -14,3 +15,9 @@ print(app.url_map)
 @app.route('/index')
 def main():
     return render_template('index.html', ip=request.remote_addr)
+
+@app.route('/ping/<target>')
+def ping(target):
+    result = requests.get("http://localhost:5000/ping/" + target)
+    result = result.json()
+    return render_template('ping.html', json=result)

a2bb8c07fd509f23dee9d1b6a7480b8ad876b6dd
Téo Le Dozo <teo@couaque.fr>
2020-02-18 10:19:55 +0000

    Integrated Bootstrap. Created first homepage template, showing the IP of the visitor.

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 #This is the main file for the vulnerability scanning API.
 #You'll find that this file is very small. It's only job is to initialize the Flask server,
 #and to respond to requests coming to the root URL (/)
-from flask import Flask, escape, request, Response
+from flask import Flask, render_template, request
 
 #We need this for the Flask server to run
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 #Printing the route map for debugging purposes
 print(app.url_map)
 
 #Code executed when someone reached out to the / path
 @app.route('/')
+@app.route('/index')
 def main():
-    return Response('Root directory of the API !')
+    return render_template('index.html', ip=request.remote_addr)

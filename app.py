580f4204e75601d823a49c521296146538a615c1
Téo Le Dozo <teo@couaque.fr>
2020-03-03 13:54:40 +0000

    Added configuration file and implemented it in the code

--- app.py
+++ app.py
@@ -2,7 +2,14 @@
 #You'll find that this file is very small. It's only job is to initialize the Flask server,
 #and to respond to requests coming to the root URL (/)
 from flask import Flask, render_template, request
-import json, requests
+import json, requests, configparser
+
+config = configparser.ConfigParser()
+config.read('configuration.ini')
+
+global api_url
+api_url = config['DEFAULT']['api_url']
+print(api_url)
 
 #We need this for the Flask server to run
 app = Flask(__name__, template_folder='templates')
@@ -18,24 +25,25 @@ def main():
 
 @app.route('/ping/<target>')
 def ping(target):
-    result = requests.get("http://localhost:5000/ping/" + target)
+    print("Calling : " + api_url + "/ping/" + target)
+    result = requests.get(api_url + "/ping/" + target)
     result = result.json()
     return render_template('ping.html', json=result, message_list=['Ping command done !'])
 
 @app.route('/portscan/<target>')
 def portscan(target):
-    result = requests.get("http://localhost:5000/portscan/" + target)
+    result = requests.get(api_url + "/portscan/" + target)
     result = result.json()
     return render_template('portscan.html', json=result, message_list=['Portscan done !'])
 
 @app.route('/servicescan/<target>/<ports>')
 def servicescan(target, ports):
-    result = requests.get("http://localhost:5000/servicescan/" + target + "/" + ports)
+    result = requests.get(api_url + "/servicescan/" + target + "/" + ports)
     result = result.json()
     return render_template('servicescan.html', json=result, message_list=['Service scan done !'])
 
 @app.route('/cipherscan/<target>')
 def cipherscan(target):
-    result = requests.get("http://localhost:5000/cipherscan/" + target)
+    result = requests.get(api_url + "/cipherscan/" + target)
     result = result.json()
     return render_template('cipherscan.html', json=result, message_list=['Cipher scan done !'])

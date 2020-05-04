0b332be486e9f62cd792a03001bdd6a49c99438a
Téo Le Dozo <teo@couaque.fr>
2020-05-04 18:24:30 +0200

    Modified the install script and Dockerfile to work on Docker AND regular OSes.

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 apt install -y python3 python3-pip
 
 pip3 install flask xmltodict ujson requests gunicorn Flask-WeasyPrint WeasyPrint
+
+apt update && apt full-upgrade -y
+apt install -y python3-pip
+apt install -y python3-flask libcairo2 libglib2.0-0
+apt install -y build-essential python3-dev python3-pip python3-setuptools python3-wheel python3-cffi libcairo2 libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info
+pip3 install flask xmltodict ujson requests gunicorn
